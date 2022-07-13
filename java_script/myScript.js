d68ff31b57dba75ab1c837c8d40852a2a090be91
ravindrakumar2907 <ravindramore6@gmail.com>
2022-07-13 22:27:22 +0530

    javascript code

---
+++ java_script/myScript.js
@@ -0,0 +1,21 @@
+//myScript.js
+/**/
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Paragraph changed.";
+}
+
+Where to write JavaScript code for testing:
+1. In browser open any Window
+2. right -> click 
+3. Choose inspect element
+4. select console tab
+5. try executing console.log("Hello")
+
+
+const is used for declaring constant value.
+
+const a = 10;
+const b = 20;
+const c = a + b;
+console.log("The result is " + c);
